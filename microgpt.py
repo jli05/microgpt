56ef4f61561b9c224d7ff1ad4b5b70eba8691903
Jencir Lee <jl@brief-ds.com>
2026-04-15 16:12:58 +0100

    add outdict vs lm_head

--- microgpt.py
+++ microgpt.py
@@ -47,7 +47,8 @@ state_dict = {'wte': matrix(vocab_size, n_embd),
               'm4': matrix(n_state, n_state),
               'token_proj': matrix(n_embd, n_state),
               'pos_proj': matrix(n_embd, n_state),
-              'lm_head': matrix(vocab_size, n_state)}
+              'lm_head': matrix(n_state, n_embd),
+              'outdict': matrix(vocab_size, n_embd)}
 params = list(state_dict.values())
 print(f"num params: {len(params)}")
 
@@ -71,7 +72,7 @@ for j in range(block_size):
     b = (b + h.attend(args) @ state_dict['m3'].attend(args)
          + b.attend(args) @ state_dict['m4'].attend(args))
 
-    delta = state_dict['lm_head'] - (h - b)
+    delta = state_dict['outdict'] - (h - b).relu() @ state_dict['lm_head']
     logits = - (delta ** 2).mean(axis=-1) ** .5
     logits_lst.append(logits)
     losses.append(- logits.softmax().attend(target_id).log())
